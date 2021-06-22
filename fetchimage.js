db811b7a057c83a75fc70866bb89eb8551989e4e
Chris Pangburn <cpangburn@gmail.com>
2021-06-22 18:27:11 +0100

    Replaced 'request' package with https request

--- fetchimage.js
+++ fetchimage.js
@@ -25,18 +25,14 @@ var download = function(uri, filename, callback = () => {}){
             });
         }).on('error', function(err) { // Handle errors
             fs.unlink(file); // Delete the file async. (But we don't check the result)
-            // if (cb){
-            //     cb(err.message);
-            // }else{
-                throw new Error(err);
-            //}
+            throw new Error(err);
         });
     });
 };
 
 //USAGE
 // (async () => { 
-//     let res = await download('https://scontent.cdninstagram.com/v/t51.29350-15/175966488_130280339116544_2553575231065349854_n.jpg?_nc_cat=109&ccb=1-3&_nc_sid=8ae9d6&_nc_ohc=xkIV-3Tbz10AX-AN6KO&_nc_ht=scontent.cdninstagram.com&oh=546edd22bb5e827942dba248ccf7cb36&oe=60D7AF3C','test.jpg');
+//     let res = await download('https://www.####.com/test.jpg');
 //     console.log(res);
 // })();
 
